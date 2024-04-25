b6398162fde79f46b19282b6c1f3933b3959c842
John-Nathan <John-Nathan@LAPTOP-TGVCIGB0>
2024-04-25 11:19:12 +0800

    Add project files.

---
+++ ToDo/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoDemo.Models
+{
+    public class Category
+    {
+        public string CategoryId { set; get; } = string.Empty;
+        public string Name { set; get; } = string.Empty;
+    }
+
+}
